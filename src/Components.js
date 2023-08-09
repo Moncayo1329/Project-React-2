6da00a63cdea076a59a395acbe314db4cd53dd93
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-08-09 18:15:19 +0100

    first commit

---
+++ src/Components.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Components() {
+    return (
+      <nav>
+<img src="../images/airbnb.png"/>
+
+
+      </nav>
+    );
+  }
+  
+ export default Components ;
