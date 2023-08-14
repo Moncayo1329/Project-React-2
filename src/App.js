d28f9f1945d0dfb2663d97244d06717dbea04bfb
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-08-14 18:41:23 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Components from './Components';
+import React from 'react';
+import airbnb from '../src/Images/airbnb.png'
+import Hero  from './Hero';
+
 import './App.css';
 
+
+
 function App() {
+
   return (
-    <div className="App">
-     <Components />
-    </div>
+    <div className='foto'>
+<img src={airbnb}></img>
+   
+
+  <Hero/>
+  </div>
+
+
   );
 }
 
---
+++ src/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Hero() {
+
+    return (
+     <section>
+  <h1>Online Experiences</h1>
+<p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+
+
+
+     </section>
+    );
+  }
+  
+  export default Hero;
